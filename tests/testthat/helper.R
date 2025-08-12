b2273329d8c42f69f82cec2e67891e779a516e04
john <bblodfon@gmail.com>
2025-08-12 16:28:57 +0200

    fix tests using the new R6 class

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -4,7 +4,3 @@ gen_surv_mat = function(nrows, ncols) {
   # Sort each row in decreasing order to make it a survival matrix
   t(apply(x, 1, function(row) sort(row, decreasing = TRUE)))
 }
-
-gen_mat = function(nrows, ncols) {
-  x = matrix(runif(nrows * ncols), nrow = nrows, ncol = ncols)
-}
--- tests/testthat/test_survDistr.R
+++ tests/testthat/test_survDistr.R
@@ -7,65 +7,64 @@
 set.seed(42)
 x = gen_surv_mat(nrows = 2, ncols = 3)
 colnames(x) = c(12, 34, 42)
-obj = survDistr(x)
+obj = survDistr$new(x)
 
 test_that("constructor", {
   # Valid input
-  expect_s3_class(obj, "survDistr")
-  expect_equal(obj$data, x)
+  checkmate::expect_r6(obj, "survDistr")
+  expect_equal(obj$data_type, "surv")
+  expect_equal(obj$get_data, x)
+  expect_equal(obj$times, c(12, 34, 42))
+  expect_equal(obj$inter_type, "const_surv")
 
   # Invalid inputs
-  expect_error(survDistr(x = NULL), "Must be of type")
-  expect_error(survDistr(x = "not a matrix"), "Must be of type")
-  expect_error(survDistr(x = x[, 0, drop = FALSE]), "Must have at least 1 cols")
-  expect_error(survDistr(x = matrix()), "Must have colnames")
-  expect_error(survDistr(x = matrix(dimnames = list(NULL, 1))), "Contains missing values")
-})
-
-test_that("times", {
-  expect_equal(times(obj), c(12, 34, 42))
+  expect_error(survDistr$new(x = NULL), "Must be of type")
+  expect_error(survDistr$new(x = "not a matrix"), "Must be of type")
+  expect_error(survDistr$new(x = x[, 0, drop = FALSE]), "Must have at least 1 cols")
+  expect_error(survDistr$new(x = matrix()), "Must have colnames")
+  expect_error(survDistr$new(x = matrix(dimnames = list(NULL, 1))), "Contains missing values")
 })
 
 test_that("constant survival", {
   # Case: `new_times` entirely before `times`
-  res = survival(obj, times = 5)
+  res = obj$survival(times = 5)
   expect_matrix(res, nrows = 2, ncols = 1, col.names = "named")
   expect_equal(res[, 1], c(1, 1))
 
   # Case: `new_times` entirely after `times`
-  res = survival(obj, times = 50)
+  res = obj$survival(times = 50)
   expect_matrix(res, nrows = 2, ncols = 1, col.names = "named")
-  expect_equal(res[, 1], obj$data[, 3]) # S(t) = S(t_max)
+  expect_equal(res[, 1], obj$get_data[, 3]) # S(t) = S(t_max)
 
-  # Case: Mixed `new_times`
-  res = survival(obj, times = c(13, 38, 45)) # each time is a bit after the one we have
+  # Case: Mixed `new_times` (each time point is a bit after the one we have)
+  res = obj$survival(times = c(13, 38, 45))
   expect_matrix(res, nrows = 2, ncols = 3, col.names = "named")
-  expect_equal(unname(res), unname(obj$data))
+  expect_equal(unname(res), obj$data)
 
   # Case: Empty `new_times`
-  res = survival(obj, times = NULL)
-  expect_equal(res, obj$data) # Returns original matrix
+  res = obj$survival(times = NULL)
+  expect_equal(res, obj$get_data) # Returns original matrix
 })
 
 test_that("linear survival", {
   obj$inter_type = "linear_surv"
 
   # Case: `new_times` entirely before `times`
-  res = survival(obj, times = 5)
+  res = obj$survival(times = 5)
   expect_matrix(res, nrows = 2, ncols = 1, col.names = "named")
   expect_true(all(res[, 1] < 1))
 
   # Case: `new_times` entirely after `times`
-  res = survival(obj, times = 50)
+  res = obj$survival(times = 50)
   expect_matrix(res, nrows = 2, ncols = 1, col.names = "named")
-  expect_true(all(res[, 1] < obj$data[, 3])) # S(t) < S(t_max)
+  expect_true(all(res[, 1] < obj$get_data[, 3])) # S(t) < S(t_max)
 
   # Case: Mixed `new_times`
-  res = survival(obj, times = c(13, 38, 45)) # each time is a bit after the one we have
+  res = obj$survival(times = c(13, 38, 45)) # each time is a bit after the one we have
   expect_matrix(res, nrows = 2, ncols = 3, col.names = "named")
   expect_true(all(res < obj$data)) # Linear interpolation within range
 
   # Case: Empty `new_times`
-  res = survival(obj, times = NULL)
-  expect_equal(res, obj$data) # Returns original matrix
+  res = obj$survival(times = NULL)
+  expect_equal(res, obj$get_data) # Returns original matrix
 })
