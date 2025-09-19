b22a45cac7e53f5b9c6204714d3befab26eef990
john <bblodfon@gmail.com>
2025-09-19 03:49:39 +0300

    import assert_flag

--- R/survdistr-package.R
+++ R/survdistr-package.R
@@ -1,7 +1,5 @@
 ## usethis namespace: start
-#' @importFrom checkmate assert_choice
-#' @importFrom checkmate assert_matrix
-#' @importFrom checkmate assert_numeric
+#' @importFrom checkmate assert_choice assert_matrix assert_numeric assert_flag
 #' @importFrom Rcpp sourceCpp
 #' @importFrom R6 R6Class
 #' @useDynLib survdistr, .registration = TRUE
