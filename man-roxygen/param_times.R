928c543b00b5a85a8f13144c23c53ab3dbb59afb
john <bblodfon@gmail.com>
2025-09-21 22:23:50 +0300

    refine doc

--- man-roxygen/param_times.R
+++ man-roxygen/param_times.R
@@ -1,4 +1,4 @@
 #' @param times (`numeric`)\cr
 #' New time points at which to interpolate.
 #' Values do not need to be sorted or unique, just non-negative.
-#' If `NULL`, the input object's stored time points are used.
+#' If `NULL`, the object's stored time points are used.
