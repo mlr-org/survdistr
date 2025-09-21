a0aa0f14b520b82c3f9565e70b7a563fb1f995dd
john <bblodfon@gmail.com>
2025-09-21 21:01:20 +0300

    better handling of assertions and NULL eval_times

--- R/interp_funs.R
+++ R/interp_funs.R
@@ -18,8 +18,7 @@
 #'  If `FALSE`, use piecewise-linear interpolation.
 #' @param type (`character(1)`)\cr
 #'  One of `"surv"`, `"cdf"`, or `"cif"`, indicating the data type of the input matrix.
-#' @param add_times (`logical(1)`)\cr
-#'  If `TRUE`, sets the column names of the returned matrix to the requested `eval_times`.
+#' @template param_add_times
 #' @param check (`logical(1)`)\cr
 #'  If `TRUE`, run input matrix validation via [assert_surv_matrix()];
 #'  set `FALSE` to skip checks (NOT recommended for external use).
@@ -28,33 +27,39 @@
 #'   columns equal to `length(eval_times)`.
 #'
 #' @examples
-#' x = matrix(c(1,0.8,0.6,
-#'              1,0.7,0.4), nrow = 2, byrow = TRUE)
+#' x = matrix(c(1, 0.8, 0.6,
+#'              1, 0.7, 0.4),
+#'            nrow = 2, byrow = TRUE)
 #' times = c(0, 10, 20)
 #' eval_times = c(5, 15, 25, 15)  # duplicates & unordered
 #' mat_interp(x, times, eval_times, constant = TRUE, type = "surv")
 #' @export
 mat_interp = function(x, times = NULL, eval_times = NULL, constant = TRUE, type = "surv",
                       add_times = TRUE, check = TRUE) {
-  # general checks
-  eval_times = assert_numeric(eval_times, lower = 0, any.missing = FALSE,
-                              null.ok = TRUE, min.len = 1)
-  if (is.null(eval_times)) {
-    return(x)
-  }
+  # quick assertions
   assert_flag(constant)
   type = assert_choice(type, c("surv", "cdf", "cif"))
   assert_flag(add_times)
   assert_flag(check)
+  eval_times = assert_numeric(eval_times, lower = 0, any.missing = FALSE,
+                              null.ok = TRUE, min.len = 1)
 
   # Optional matrix check
   if (check) {
-    if (type == "surv") {
-      times = assert_surv_matrix(x, times)
-    } else {
-      # Transform cdf/cif to survival for checks
-      times = assert_surv_matrix(1 - x, times)
+    times = assert_prob_matrix(x, times, type = type)
+  }
+
+  # case: no interpolation requested
+  if (is.null(eval_times)) {
+    if (add_times && is.null(colnames(x))) {
+      # derive the times if possible
+      if (check ||
+          test_numeric(times, lower = 0, unique = TRUE, len = ncol(x),
+                       sorted = TRUE, any.missing = FALSE, null.ok = FALSE)) {
+        colnames(x) = as.character(times)
+      }
     }
+    return(x)
   }
 
   # unique + sorted eval_times for C++
