7705beb12a9b253cb56bf2085b5d7079e4ba98c9
john <bblodfon@gmail.com>
2026-03-05 15:53:31 +0100

    more tests for survDistr

--- tests/testthat/test_survDistr.R
+++ tests/testthat/test_survDistr.R
@@ -1,15 +1,9 @@
-# TODO Notes:
-# compare linear vs constant (exact values) on the vectorized version
-# new_times values entirely before times.
-# new_times values entirely after times.
-# Mixed new_times values spanning the range of times.
-# Check the behavior for both constant and linear interpolation.
 set.seed(42)
-x = gen_random_mat(nrows = 2, ncols = 3, type = "surv")
+x = gen_random_mat(nrows = 3, ncols = 3, type = "surv")
 times = c(12, 34, 42)
 obj = survDistr$new(x, times)
 
-test_that("constructor works", {
+test_that("new() works", {
   # Valid input
   checkmate::expect_r6(obj, "survDistr")
   expect_equal(obj$data(add_times = FALSE), x)
@@ -24,22 +18,26 @@ test_that("constructor works", {
   expect_error(survDistr$new(x = matrix(dimnames = list(NULL, 1))), "Contains missing values")
 })
 
+test_that("print() works", {
+  expect_output(print(obj), "survival matrix")
+})
+
 test_that("filter() works", {
   obj2 = obj$clone(deep = TRUE)
 
   # can't filter out of bounds (2 observations only)
   expect_error(obj2$filter(rows = c(0, 2)), ">= 1")
-  expect_error(obj2$filter(rows = c(1, 4)), "<= 2")
-  expect_error(obj2$filter(rows = c(FALSE, TRUE, FALSE)), "Must have length 2")
+  expect_error(obj2$filter(rows = c(1, 4)), "<= 3")
+  expect_error(obj2$filter(rows = c(FALSE, TRUE)), "Must have length 3")
   expect_error(obj2$filter(rows = c(1, 1)), "duplicated values")
   expect_error(obj2$filter(rows = c(2, 1)), "be sorted")
 
+  # filter to 2 observations
+  expect_invisible(obj2$filter(rows = c(1, 3)))
+  expect_equal(obj2$data(add_times = FALSE), x[c(1, 3), , drop = FALSE])
   # filter to 1 observation
-  expect_invisible(obj2$filter(rows = 1))
-  expect_equal(obj2$data(add_times = FALSE), x[1, , drop = FALSE])
-  # no change (keep the only observation)
-  obj2$filter(rows = TRUE)
-  expect_equal(obj2$data(add_times = FALSE), x[1, , drop = FALSE])
+  obj2$filter(rows = c(FALSE, TRUE))
+  expect_equal(obj2$data(add_times = FALSE), x[3, , drop = FALSE])
   # remove last observation
   obj2$filter(rows = FALSE)
   expect_equal(dim(obj2$data(add_times = FALSE)), c(0, 3))
@@ -47,47 +45,57 @@ test_that("filter() works", {
   expect_equal(obj$data(add_times = FALSE), x)
 })
 
-test_that("constant survival", {
-  # Extrapolation case 1: requested time before earliest time
-  res = obj$survival(times = 5)
-  expect_matrix(res, nrows = 2, ncols = 1, col.names = "named")
-  expect_equal(res[, 1], c(1, 1)) # S(t < t_min) = 1
+test_that("subsetting using 'rows' works", {
+  obj3 = obj$clone(deep = TRUE)
 
-  # Extrapolation case 2: requested time after latest time
-  res = obj$survival(times = 50)
-  expect_matrix(res, nrows = 2, ncols = 1, col.names = "named")
-  expect_equal(res[, 1], obj$data()[, 3]) # S(t > t_max) = S(t_max)
-
-  # Interpolation case: requested time points are a bit after the original ones we have
-  res = obj$survival(times = c(13, 38, 45), add_times = FALSE)
-  expect_matrix(res, nrows = 2, ncols = 3, col.names = "unnamed")
-  expect_equal(res, obj$data(add_times = FALSE)) # equal due to left-continuous interpolation
-
-  # Case: Empty `new_times`
-  res = obj$survival(times = NULL)
-  expect_equal(res, obj$data()) # Returns original matrix
+  expect_equal(obj3$data(rows = 1, add_times = FALSE), x[1, , drop = FALSE])
+  expect_equal(obj3$survival(rows = c(1, 3), add_times = FALSE), x[c(1, 3), , drop = FALSE])
+  expect_equal(obj3$cdf(rows = c(1, 3), add_times = FALSE), 1 - x[c(1, 3), , drop = FALSE])
+  # obj3 remains unchanged
+  expect_equal(obj3$data(add_times = FALSE), x)
 })
 
-test_that("linear survival", {
-  # can 'hack' the interpolation method
-  obj$interp_meth = "linear_surv"
+test_that("survival() works", {
+  obj2 = obj$clone(deep = TRUE)
 
-  # Extrapolation case 1: requested time before earliest time
-  res = obj$survival(times = 5)
-  expect_matrix(res, nrows = 2, ncols = 1, col.names = "named")
-  expect_true(all(res[, 1] < 1)) # S(t > t_max) = 1
+  # constant survival interpolation (default)
+  t = c(0, 7, 12, 22, 34, 40, 42, 50)
+  res = obj2$survival(times = t)
+  expect_matrix(res, nrows = 3, ncols = length(t), col.names = "named")
+  res2 = obj2$survival()
+  expect_equal(res2, obj2$data()) # Returns original matrix
 
-  # Extrapolation case 2: requested time after latest time
-  res = obj$survival(times = 50)
-  expect_matrix(res, nrows = 2, ncols = 1, col.names = "named")
-  expect_true(all(res[, 1] < obj$data()[, 3])) # S(t > t_max) < S(t_max)
+  # linear survival interpolation
+  obj2$interp_meth = "linear_surv"
+  res3 = obj2$survival(times = t)
+  time_cols = as.character("0", "12", "34", "42")
+  expect_equal(res3[, time_cols], res[, time_cols]) # no interpolation at original time points
+})
 
-  # Interpolation case: requested time points are a bit after the original ones we have
-  res = obj$survival(times = c(13, 38, 45))
-  expect_matrix(res, nrows = 2, ncols = 3, col.names = "named")
-  expect_true(all(res < obj$data()))
+test_that("cdf() works", {
+  t = c(0, 7, 12, 22, 34, 40, 42, 50)
+  res = obj$cdf(times = t)
+  expect_matrix(res, nrows = 3, ncols = length(t), col.names = "named")
 
-  # Case: Empty `new_times`
-  res = obj$survival(times = NULL)
-  expect_equal(res, obj$data()) # Returns original matrix
+  # Returns original matrix transformed to CDF
+  res2 = obj$cdf()
+  expect_equal(res2, 1 - obj$data())
+})
+
+test_that("cumhazard() works", {
+  obj2 = obj$clone(deep = TRUE)
+  obj2$interp_meth = "linear_surv" # H(t) grows
+
+  t = c(0, 7, 12, 22, 34, 40, 42, 50)
+  res = obj2$cumhazard(times = t)
+  expect_matrix(res, nrows = 3, ncols = length(t), col.names = "named")
+  expect_equal(res[,1], c(0, 0, 0)) # H(0) = 0
+  expect_all_true(res[1, ] >= 0) # H(t) increases
+  expect_all_true(res[2, ] >= 0)
+  expect_all_true(res[3, ] >= 0)
+
+  # large times so that eps kicks in
+  res2 = obj2$cumhazard(times = 1000)
+  res3 = obj2$cumhazard(times = 1000, eps = 1e-10) # lower eps => lower S(t) => higher H(t)
+  expect_all_true(res2[,1] < res3[,1])
 })
