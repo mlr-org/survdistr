74b7d759d5bf6869ab9aa104dbf4af6495478286
john <bblodfon@gmail.com>
2026-03-10 14:28:16 +0100

    refine docs

--- man-roxygen/param_check.R
+++ man-roxygen/param_check.R
@@ -1,4 +1,4 @@
 #' @param check (`logical(1)`)\cr
-#'  If `TRUE`, run input matrix validation checks;
+#'  If `TRUE`, run input matrix validation checks using [assert_prob()];
 #'  set to `FALSE` to skip checks (NOT recommended for external use).
 #'
